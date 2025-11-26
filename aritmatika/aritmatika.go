691a300bec9e8c1dbe29d6e40e04a065c3d0d16f
Seno Dwi Prasetyo <senothoha77@gmail.com>
2025-11-26 19:10:44 +0700

    variabel + aritmatika

---
+++ aritmatika/aritmatika.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var hasilTambah int = 10 + 20
+	hasilKurang := 40 - 20
+	hasilKali := 20 * 20
+	var hasilBagi int = 10 / 2
+	hasilModulus := 100 % 3
+
+	fmt.Println(hasilTambah)
+	fmt.Println(hasilKurang)
+	fmt.Println(hasilKali)
+	fmt.Println(hasilBagi)
+	fmt.Println(hasilModulus)
+}
--- variabel/variabel.go
+++ variabel/variabel.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main(){
-	var name string = "Seno Dwi Prasetyo"
-	age := 56
-	age = 100
+	//deklarasi variabel
+	// var -> perlu menulis tipe datanya
+	// := -> tidak perlu menulis tipe datanya
+	// uint -> hanya support bilangan positif
+	var number int64 = 12345
+	number_2 := 12345
 
-	fmt.Println(name)
-	fmt.Println(age)
+	fmt.Println(number_2)
+
+	number_2 = 123
+
+	fmt.Println(number)
+	fmt.Println(number_2)
+
+	var yes bool = true
+	no := false
+
+	no = true
+	fmt.Println(no) 
+	fmt.Println(yes)
 }
